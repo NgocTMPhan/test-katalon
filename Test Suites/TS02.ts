87b40182fef2178415c2079dd4b472e1c3f944e5
Ngoc Tran Minh Phan <ngoctmphan@kms.com.vn>
2019-09-10 16:12:30 +0700

    update

---
+++ Test Suites/TS02.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS02</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>ad02ac01-e929-4d05-a119-717485fcc48a</testSuiteGuid>
+   <testCaseLink>
+      <guid>2e1bfb08-588f-4f1b-bab3-697d382c7d0e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ngoc01</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>403a202a-696c-4fd3-bb1d-317f4fa519ba</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ngoc02</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
