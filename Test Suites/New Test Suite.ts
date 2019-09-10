86eef97255a4cd7236ece45530cb68ec84f06e7a
Ngoc Tran Minh Phan <ngoctmphan@kms.com.vn>
2019-09-10 16:08:31 +0700

    test

---
+++ Test Suites/New Test Suite.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>ac7eded6-eee4-4e14-9402-872774d3c1aa</testSuiteGuid>
+   <testCaseLink>
+      <guid>8f0ef23e-c8d1-4f3d-b158-9061dd69b212</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/New Test Case</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
